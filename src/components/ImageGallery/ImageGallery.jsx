ae392796ce48045f3dde3ad75b1cc0a097f59466
Yurii Kovtunenko <kovtunenko.91.09@gmail.com>
2022-01-27 22:57:31 +0200

    Adds a solution to the task

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import ImageGalleryItem from 'components/ImageGalleryItem';
+import { Gallery } from './ImageGallery.styled';
+
+export default function ImageGallery({ images, onImageClick }) {
+  return (
+    <Gallery>
+      {images.map(({ id, largeImageURL, webformatURL, tags }) => {
+        return (
+          <ImageGalleryItem
+            key={id}
+            picture={webformatURL}
+            largeImageURL={largeImageURL}
+            tags={tags}
+            onOpenImage={onImageClick}
+          />
+        );
+      })}
+    </Gallery>
+  );
+}
+
+ImageGallery.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      webformatURL: PropTypes.string,
+    }).isRequired
+  ),
+  onImageClick: PropTypes.func.isRequired,
+};
